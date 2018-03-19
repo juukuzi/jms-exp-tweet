e1331998f704781b87ba0bc265a4ed6de9b2e072
juukuzi <juukuzi@gmail.com>
2018-03-19 20:38:41 +0900

    spring boot initialization

---
+++ src/main/kotlin/xyz/juukuzi/jmsexptweet/JmsexptweetApplication.kt
@@ -0,0 +1,11 @@
+package xyz.juukuzi.jmsexptweet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JmsexptweetApplication
+
+fun main(args: Array<String>) {
+    runApplication<JmsexptweetApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/juukuzi/jmsexptweet/JmsexptweetApplicationTests.kt
@@ -0,0 +1,16 @@
+package xyz.juukuzi.jmsexptweet
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class JmsexptweetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
